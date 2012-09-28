bc2eae95767a0a88f75ceb880c1cf67e8cd699ea
Siôn le Roux <sinisterstuf@gmail.com>
2012-09-28 17:46:15 +0200

    add EPL copyright notice to java source files

--- src/org/sinisterstuf/wors/ApplicationWorkbenchAdvisor.java
+++ src/org/sinisterstuf/wors/ApplicationWorkbenchAdvisor.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Siôn Le Roux and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.sinisterstuf.wors;
 
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
